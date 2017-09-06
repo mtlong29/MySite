865f1ba622b2c5f3d7a67c6443c3c1d5a5a9032e
Matthew Long <long.matthew29@gmail.com>
2017-09-06 14:57:33 -0400

    ..

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,101 +1,88 @@
 $(document).ready(function () {
 
-	// toggle overlay navigation
-	$('.overlayMenuTrigger').on('click', function () {
-		// in Firefox transitions break when parent overflow is changed, so we need to wait for the end of the transition to give the body an overflow hidden
-		if ($('.overlayMenu').hasClass('is--visible')) {
-			$('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-			$('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-		} else {
-			$('.overlayMenu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-			$('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-		}
-	});
+  // Toggle Overlay Navigation
+  $('.overlayMenuTrigger').on('click', function () {
+    if ($('.overlayMenu').hasClass('is--visible')) {
+      $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    } else {
+      $('.overlayMenu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      $('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    }
+  });
+  // Close Overlay Navigation On Button Click
+  $('.overlayMenuClose, #screen').on('click', function () {
+    $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+  });
+  // Toggle Menu Overlay Navigation On Focus
+  $('.overlayMenuItem a').on('focus', function () {
+    $('.overlayMenu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+  });
+  $('.overlayMenuClose').on('focus', function () {
+    $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+  });
+  // Close Menu On [esc]
+  $(document).on('keydown', function (e) {
+    if (e.keyCode === 27) { // ESC key
+      $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+    }
+  }); // End Toggle Overlay Navigation
 
-	// close overlay navigation on button click
-	$('.overlayMenuClose, #screen').on('click', function () {
-		$('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-		$('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-	});
+  // Move Table of Contents From Post Body to Sidebar
+  $(".post__body .js-toc").appendTo($(".toc--sidebar")).hide().fadeIn(400);
 
-	// open/close overlay navigation on focus
-	$('.overlayMenuItem a').on('focus', function () {
-		$('.overlayMenu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-		$('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-	});
-	$('.overlayMenuClose').on('focus', function () {
-		$('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-		$('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-	});
+  // Sticky Sidebar Initalize
+  $(".sticky").Stickyfill();
 
-	// close menu on [esc]
-	$(document).on('keydown', function (e) {
-		if (e.keyCode === 27) { // ESC key
-			$('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-			$('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
-		}
-	});
+  // FitVids Initialize
+  $("#main").fitVids();
 
+  // Smooth Scroll Initialize
+  $("a").smoothScroll({
+    offset: -20
+  });
 
-	// move table of contents from post body to sidebar
-	$(".post__body .js-toc").appendTo($(".toc--sidebar")).hide().fadeIn(400);
-
-
-	// sticky sidebar
-	$(".sticky").Stickyfill();
-
-
-	// FitVids init
-	$("#main").fitVids();
-
-
-	// smooth scroll init
-	$("a").smoothScroll({
-		offset: -20
-	});
-
-	// add lightbox class to all image links
-	$("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+  // Add Lightbox Class to All Image Links
+  $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
 
-	// Magnific-Popup options
-	$('.image-popup').magnificPopup({
-		disableOn: function () {
-			if ($(window).width() < 500) {
-				return false;
-			}
-			return true;
-		},
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
-		},
-		removalDelay: 500, // Delay in milliseconds before popup is removed
-		// Class that is added to body when popup is open.
-		// make it unique to apply your CSS animations just to this exact popup
-		mainClass: 'mfp-zoom-in',
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-	});
+  // Magnific-Popup Settings
+  $('.image-popup').magnificPopup({
+    disableOn: function () {
+      if ($(window).width() < 500) {
+        return false;
+      }
+      return true;
+    },
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 500,
+    mainClass: 'mfp-zoom-in',
+    callbacks: {
+      beforeOpen: function () {
+        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+      }
+    },
+    closeOnContentClick: true,
+    midClick: true
+  }); // End Magnific-Popup Settings
 
-
-	// lazy load settings
-	$("img.load").show().lazyload({
-		effect: "fadeIn",
-		skip_invisible: false
-	});
-
+  // Lazy Load Settings
+  $("img.load").show().lazyload({
+    effect: "fadeIn",
+    skip_invisible: false
+  }); // End Lazy Load Settings
 
 });
