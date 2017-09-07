659d26aa0550259610fce683feceb6e133e14f71
Matthew Long <long.matthew29@gmail.com>
2017-09-06 20:02:13 -0400

    updates to main.js
    Added settings for various jquery plugins, removed some old jquery plugins, added arrow functions, and added target="_blank" using jquery

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
   // Toggle Overlay Navigation
-  $('.overlayMenuTrigger').on('click', function () {
+  $('.overlayMenuTrigger').on('click', () => {
     if ($('.overlayMenu').hasClass('is--visible')) {
       $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
       $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
@@ -11,48 +11,70 @@ $(document).ready(function () {
     }
   });
   // Close Overlay Navigation On Button Click
-  $('.overlayMenuClose, #screen').on('click', function () {
+  $('.overlayMenuClose, #screen').on('click', () => {
     $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
     $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
   });
   // Toggle Menu Overlay Navigation On Focus
-  $('.overlayMenuItem a').on('focus', function () {
+  $('.overlayMenuItem a').on('focus', () => {
     $('.overlayMenu').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
     $('#screen').addClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
   });
-  $('.overlayMenuClose').on('focus', function () {
+  $('.overlayMenuClose').on('focus', () => {
     $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
     $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
   });
   // Close Menu On [esc]
-  $(document).on('keydown', function (e) {
+  $(document).on('keydown', e => {
     if (e.keyCode === 27) { // ESC key
       $('.overlayMenu').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
       $('#screen').removeClass('is--visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
     }
   }); // End Toggle Overlay Navigation
 
+  // Add Target Equals Blank to Out of Site Links
+  $('a:not([href*="http://localhost:4000"],[href*="http://www.mtlong.me/"])').attr("target", "_blank");
+
   // Move Table of Contents From Post Body to Sidebar
-  $(".post__body .js-toc").appendTo($(".toc--sidebar")).hide().fadeIn(400);
+  $('.post__body .js-toc').appendTo($('.toc--sidebar')).hide().fadeIn(600);
 
-  // Sticky Sidebar Initalize
-  $(".sticky").Stickyfill();
+  // Sticky Settings
+  const $headerHeight = $('.masthead').height();
+  const $contentsOffset = $('header').height() - $('.post__title').height();
 
-  // FitVids Initialize
-  $("#main").fitVids();
+  $('.sticky').sticky({
+    topSpacing: $headerHeight + $contentsOffset,
+    widthFromWrapper: false
+  }); // End Sticky Settings
 
-  // Smooth Scroll Initialize
-  $("a").smoothScroll({
-    offset: -20
+  // Callback Functions for Events Created by Sticky.js
+  $('.sticky').on('sticky-start', () => {
+    $('.toc').css('border-top', '0');
+    $('.toc__title').css({
+      'border-left': '8px solid #5C88C4',
+      'padding': '0 0 0 .75rem'
+    });
   });
+  $('.sticky').on('sticky-end', () => {
+    $('.toc').css('border-top', '8px solid #5C88C4');
+    $('.toc__title').css({
+      'border-left': '0',
+      'padding': '0'
+    });
+  }); // End Sticky Callback Functions
+
+  // Smooth Scroll Settings
+  $('a').smoothScroll({
+    offset: 5
+  }); // End smoothScroll Settings
 
   // Add Lightbox Class to All Image Links
-  $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+  $('a[href$=".jpg"],a[href$=".png"],a[href$=".gif"]').addClass('image-popup');
 
 
   // Magnific-Popup Settings
   $('.image-popup').magnificPopup({
-    disableOn: function () {
+    disableOn: () => {
       if ($(window).width() < 500) {
         return false;
       }
@@ -80,8 +102,8 @@ $(document).ready(function () {
   }); // End Magnific-Popup Settings
 
   // Lazy Load Settings
-  $("img.load").show().lazyload({
-    effect: "fadeIn",
+  $('img.load').show().lazyload({
+    effect: 'fadeIn',
     skip_invisible: false
   }); // End Lazy Load Settings
 
