246365d019a21cfd7548df9c1ee2a9431e760ce2
Matthew Long <long.matthew29@gmail.com>
2017-12-27 15:36:36 -0500

    changed class names

--- _dev/javascripts/sticky-settings.js
+++ _dev/javascripts/sticky-settings.js
@@ -5,7 +5,7 @@ stickyfill.js settings
 $(document).ready(() => {
   // sticky settings
   const $headerHeight = $('.masthead').height();
-  const $contentsOffset = $('header').height() - $('.post__title').height();
+  const $contentsOffset = $('header').height() - $('.page-title').height();
 
   $('.sticky').sticky({
     topSpacing: $headerHeight + $contentsOffset,
@@ -15,14 +15,14 @@ $(document).ready(() => {
   // callback functions for events created by stickyfill.js
   $('.sticky').on('sticky-start', () => {
     $('.toc').css('border-top', '0');
-    $('.toc__title').css({
+    $('.toc-tile').css({
       'border-left': '8px solid #5C88C4',
       'padding': '0 0 0 .75rem'
     });
   });
   $('.sticky').on('sticky-end', () => {
     $('.toc').css('border-top', '8px solid #5C88C4');
-    $('.toc__title').css({
+    $('.toc-tile').css({
       'border-left': '0',
       'padding': '0'
     });
