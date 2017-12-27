ffba9647e33854dcbb2f5422a49132732b72bc2e
Matthew Long <long.matthew29@gmail.com>
2017-12-27 15:36:27 -0500

    changed class name

--- _dev/javascripts/main.js
+++ _dev/javascripts/main.js
@@ -10,7 +10,7 @@ $(document).ready(() => {
   move table of contents from post body to sidebar
   ****************************************************/
 
-  $('.post__body .js-toc').appendTo($('.toc--sidebar')).hide().fadeIn(600);
+  $('.page-body .js-toc').appendTo($('.toc--sidebar')).hide().fadeIn(600);
 
   /****************************************************
   smooth scroll settings
